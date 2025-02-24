52d55abbf2488288ed6f13bca84c6b5f14a33e78
paul.pettit <me@pauljpettit.com>
2025-02-23 21:48:52 -0700

    fix: add auth route to AppRoutes

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,12 +3,14 @@ import AppLayout from "./components/layout/AppLayout"
 import { StoryGuard } from "./components/StoryGuard"
 import Index from "./pages/Index"
 import NotFound from "./pages/NotFound"
+import AuthPage from "./pages/auth"
 
 export function AppRoutes() {
   return (
     <AppLayout>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/auth" element={<AuthPage />} />
         <Route path="/stories/*" element={<StoryGuard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
