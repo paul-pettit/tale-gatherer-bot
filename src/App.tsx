785d4cdf58fa5ff1dedc83c9787483378d62c2ca
paul.pettit <me@pauljpettit.com>
2025-02-23 21:44:53 -0700

    fix: add QueryClientProvider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import { BrowserRouter } from "react-router-dom"
 import { Toaster } from "sonner"
 import { AppRoutes } from "./AppRoutes"
 import { ConvexClientProvider } from "./convex/ConvexClientProvider"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <ConvexClientProvider>
-      <BrowserRouter>
-        <Toaster richColors position="top-center" />
-        <AppRoutes />
-      </BrowserRouter>
-    </ConvexClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ConvexClientProvider>
+        <BrowserRouter>
+          <Toaster richColors position="top-center" />
+          <AppRoutes />
+        </BrowserRouter>
+      </ConvexClientProvider>
+    </QueryClientProvider>
   )
 }
 
